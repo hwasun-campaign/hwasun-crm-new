46bac96ad9402aaa496fa174071fb2653ab6a0e2
hwasun <rhgidrmfl@daum.net>
2025-04-10 13:29:18 +0900

    supabase 저장 기능 추가

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://wxdlitplwqgmapjolnup.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4ZGxpdHBsd3FnbWFwam9sbnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNDcxODYsImV4cCI6MjA1OTgyMzE4Nn0.exP52Z3w89VypSJD1rIgQXejpif1yP5jI_OV8vsAK2k';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
