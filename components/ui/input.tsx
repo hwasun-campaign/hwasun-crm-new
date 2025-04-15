2d3a9d978309e508896e2683e048f55d680a43ae
hwasun <rhgidrmfl@daum.net>
2025-04-15 13:30:36 +0900

    주소창 변경

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,5 +1,14 @@
 import { InputHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils'; // className 병합 함수 (shadcn이나 tailwind에서 자주 씀)
 
-export function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input className="border rounded p-2 w-full" {...props} />;
+export function Input({
+  className,
+  ...props
+}: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className={cn("border rounded p-2 w-full", className)}
+      {...props}
+    />
+  );
 }
